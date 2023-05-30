aa479afd460ae0fdc4fc1135660274259cd6e541
vishwa <saivishwa.gaddam.com>
2023-05-29 21:17:06 -0700

    new data ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,6 +1,5 @@
 import os
 import sys
-#sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
@@ -8,6 +7,11 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+# from src.components.model_trainer import ModelTrainerConfig
+# from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -49,6 +53,9 @@ class DataIngestion:
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
+
 
     
 
